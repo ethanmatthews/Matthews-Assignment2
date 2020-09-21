8e089c9a41ac5a65bc8170fedc0fa15971bf062e
ethanmatthews <ethan-matthews@student.kirkwood.edu>
2020-09-20 23:19:34 -0500

    Fixed Excersises 2

--- exercises.py
+++ exercises.py
@@ -11,14 +11,14 @@ Output(s):
 # Type your code here
 
 # Inputs
-# miles_driven = float(input('How many miles did you drive?'))
-# gallons_used = float(input('How many gallons of gas did you use?'))
+miles_driven = float(input('How many miles did you drive?'))
+gallons_used = float(input('How many gallons of gas did you use?'))
 
-# # Process
-# miles_per_gallon = float(miles_driven / gallons_used)
+# Process
+miles_per_gallon = float(miles_driven / gallons_used)
 
-# # Output
-# print('You used ' + format(miles_per_gallon, '.2f') + ' miles per gallon.')
+# Output
+print('You used ' + format(miles_per_gallon, '.2f') + ' miles per gallon.')
 '''
 Question 9. Celsius to Fahrenheit Temperature Converter
 Input(s): 
@@ -31,20 +31,25 @@ Output(s):
 # Type your code here
 
 # Input
-# celsius = float(input('Input Celsius Temperature.\n'))
+celsius = float(input('Input Celsius Temperature.\n'))
 
-# # Process
-# fahrenheit = float(((celsius/5)*9)+32)
+# Process
+fahrenheit = float(((celsius/5)*9)+32)
 
-# # Output
-# print('Fahrenheit is ' + format(fahrenheit, '.1f') + ' degrees.')
+# Output
+print('Fahrenheit is ' + format(fahrenheit, '.1f') + ' degrees.')
 '''
 Question 11. Male and Female Percentages
 Input(s):
-- males, int, from the user
-- 
+- males, float, from the user
+- females, float, from the user
 Process(es): 
+- total, float, males + females
+- male_percent, float, males / total
+- female_percent, float, females / total
 Output(s): 
+- male_percent
+- female_percent
 '''
 # Type your code here
 
@@ -83,28 +88,25 @@ Output(s):
   - earnings
 '''
 # Type your code here
+COMMISSION_RATE = float(0.03)
 
-# COMMISSION_RATE = float(0.03)
+# Inputs
+shares_purchased = int(input("How many shares have you purchased?"))
+purchase_price = float(input("How much did you buy each share for?"))
+shares_sold = int(input("How many shares have you sold?"))
+sale_price  = float(input("How much did you sell each share for?"))
 
-# # Inputs
-
-# shares_purchased = int(input("How many shares have you purchased?"))
-# purchase_price = float(input("How much did you buy each share for?"))
-# shares_sold = int(input("How many shares have you sold?"))
-# sale_price  = float(input("How much did you sell each share for?"))
-
-# # Processes
-
-# amount_paid = float(shares_purchased * purchase_price)
-# commision_1 = float(amount_paid * COMMISSION_RATE)
-# amount_sold = float(shares_sold * sale_price)
-# commision_2 = float(amount_sold * COMMISSION_RATE)
-# earnings = float(amount_sold - amount_paid - commision_1 - commision_2)
+# Processes
+amount_paid = float(shares_purchased * purchase_price)
+commision_1 = float(amount_paid * COMMISSION_RATE)
+amount_sold = float(shares_sold * sale_price)
+commision_2 = float(amount_sold * COMMISSION_RATE)
+earnings = float(amount_sold - amount_paid - commision_1 - commision_2)
 
 
-# # Outputs
-# print("You paid $" + format(amount_paid, ".2f") + " for " + str(shares_purchased) + " stocks!")
-# print("You had to pay your broker $" + (format(commision_1, ".2f")) + ".")
-# print("You sold all of your shares for $" + (format(amount_sold, ".2f")) + "!")
-# print("You had to pay your broker again for $" + (format(commision_2, ".2f")) + "!")
-# print("You made $"  + (format(earnings, ".2f")) + " overall!")
+# Outputs
+print("You paid $" + format(amount_paid, ".2f") + " for " + str(shares_purchased) + " stocks!")
+print("You had to pay your broker $" + (format(commision_1, ".2f")) + ".")
+print("You sold all of your shares for $" + (format(amount_sold, ".2f")) + "!")
+print("You had to pay your broker again for $" + (format(commision_2, ".2f")) + "!")
+print("You made $"  + (format(earnings, ".2f")) + " overall!")
